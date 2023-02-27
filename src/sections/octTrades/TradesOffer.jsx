47c867b76355544f9c617bcfefacb2dc9f8b5f87
dedyseptian <dedy.septian@tiket.com>
2023-02-27 12:50:42 +0700

    change fetch offer counter method

--- src/sections/octTrades/TradesOffer.jsx
+++ src/sections/octTrades/TradesOffer.jsx
@@ -1,12 +1,11 @@
+import React, { useEffect, useState, useMemo } from 'react';
+
 import { Box } from '@mui/material';
 
 import {
   useQueriesFilterMarketPlaceData,
   useQueryCounterOfferIdList,
-  useQueriesFilterCounterOfferData,
-  useQueryZeSwapIdList,
-  useQueryMyZeSwapId,
-  useQueryOfferId
+  useQueryZeSwapIdList
 } from 'hooks/react-query/queries';
 
 import MarketPlaceSection from '../MarketPlace/MarketPlaceSection';
@@ -14,22 +13,47 @@ import MarketPlaceSection from '../MarketPlace/MarketPlaceSection';
 export default function TradesOffer() {
   const { data: counterOfferIdList } = useQueryCounterOfferIdList();
   const { data: swaplist_ } = useQueryZeSwapIdList();
+  const allSwapList = useQueriesFilterMarketPlaceData(swaplist_);
+  const [filteredOffer, setFillteredOffer] = useState();
 
-  const newZeSwapList = useQueriesFilterCounterOfferData(
-    swaplist_,
-    counterOfferIdList,
-    true
-  );
+  const allFinished = useMemo(() => {
+    if (allSwapList.length !== 0) {
+      let flag = true;
 
-  /*const newZeSwapList = useQueriesFilterMarketPlaceData(
-    counterOfferIdList,
-    true
-  );*/
+      allSwapList.forEach((e) => {
+        if (e === undefined) {
+          flag = false;
+        }
+      });
 
-  const basicList = counterOfferIdList;
-  //console.log(JSON.stringify(newZeSwapList));
-  //console.log('total counter' + basicList.length);
-  //console.log(JSON.stringify(basicList));
+      return flag;
+    }
+
+    return false;
+  }, [allSwapList]);
+
+  const normalizeOfferData = (allSwapList, offerIdList) => {
+    const filterSwapBasedOnOfferIdList = [];
+
+    allSwapList.forEach((swapData) => {
+      swapData.swap?.offers.forEach((offerId) => {
+        if (offerIdList.includes(offerId)) {
+          filterSwapBasedOnOfferIdList.push(swapData);
+        }
+      });
+    });
+
+    return filterSwapBasedOnOfferIdList;
+  };
+
+  useEffect(() => {
+    if (allFinished) {
+      // all the queries have executed successfully
+      setFillteredOffer(normalizeOfferData(allSwapList, counterOfferIdList));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [allFinished]);
+
   return (
     <Box
       sx={{
@@ -37,7 +61,7 @@ export default function TradesOffer() {
       }}
     >
       <Box>
-        {newZeSwapList?.map((swapList, idx) => (
+        {filteredOffer?.map((swapList, idx) => (
           <MarketPlaceSection
             isOffer
             key={swapList?.swap_id}
